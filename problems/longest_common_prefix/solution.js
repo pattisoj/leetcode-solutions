b6ea8263ccd58b57b5c14cd1de46282d7113b8d4
Josh Pattison <103646942+pattisoj@users.noreply.github.com>
2022-11-27 11:59:52 +0000

    Sync LeetCode submission - Longest Common Prefix (javascript)

---
+++ problems/longest_common_prefix/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let longestCommonPrefix = "";
+    
+    if (strs == null || strs.length == 0) {
+        return longestCommonPrefix;
+    }
+    
+    let minimumLength = strs[0].length;
+    for (let i = 1; i < strs.length; i++) {
+        minimumLength = Math.min(minimumLength, strs[i].length);
+    }
+    
+    for (let i = 0; i < minimumLength; i++) {
+        let current = strs[0][i];
+        for (let j = 0; j < strs.length; j++) {
+            if (strs[j][i] != current) {
+                return longestCommonPrefix;
+            }
+        }
+        longestCommonPrefix += current;
+    }
+    return longestCommonPrefix;
+};
