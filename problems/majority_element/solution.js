b03e5d4d6a85e441b36e80cc2c25a7d13916ee61
Josh Pattison <103646942+pattisoj@users.noreply.github.com>
2022-12-05 12:59:28 +0000

    Sync LeetCode submission - Majority Element (javascript)

---
+++ problems/majority_element/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    var obj = {};
+    
+    for(var i = 0; i < nums.length; i++){
+        obj[nums[i]] = obj[nums[i]] + 1 || 1;
+        if(obj[nums[i]] > nums.length / 2)  return nums[i];
+    }
+    
+};
