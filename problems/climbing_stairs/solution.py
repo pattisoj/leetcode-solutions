e52d63c830ccf1eb9074d6580a6d44bba348c8f1
Josh Pattison <103646942+pattisoj@users.noreply.github.com>
2022-12-12 13:43:10 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -2,11 +2,11 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         
         if(n<2):
-            return n;
+            return n
         
-        ans = [1,2];
+        ans = [1,2]
         
         for i in range(2, n):
-            ans.append(ans[i-1]+ans[i-2]);
+            ans.append(ans[i-1]+ans[i-2])
             
-        return ans[n-1];
+        return ans[n-1]
