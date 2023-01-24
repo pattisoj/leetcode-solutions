decce812da15cc7599d0604178522ab6dcd31b63
Josh Pattison <103646942+pattisoj@users.noreply.github.com>
2023-01-24 10:49:07 +0000

    Sync LeetCode submission - Shuffle the Array (javascript)

---
+++ problems/shuffle_the_array/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let result = Array(2 * n);
+    
+    for (let i = 0; i < n; ++i) {
+        result[2 * i] = nums[i];
+        result[2 * i + 1] = nums[n + i];
+    }
+    return result;
+};
